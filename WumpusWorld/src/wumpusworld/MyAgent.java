cd332618d2bb234c9981c98d65e047ec71677a43
Patrick <dawnerz@163.com>
2019-10-24 21:42:40 +0800

    part of work

--- WumpusWorld/src/wumpusworld/MyAgent.java
+++ WumpusWorld/src/wumpusworld/MyAgent.java
@@ -1,5 +1,8 @@
 package wumpusworld;
 
+import java.nio.charset.CoderResult;
+import java.util.*;
+
 /**
  * Contains starting code for creating your own Wumpus World agent.
  * Currently the agent only make a random decision each turn.
@@ -10,6 +13,74 @@ public class MyAgent implements Agent
 {
     private World w;
     int rnd;
+
+    /**
+     * Record progress of the game
+     */
+    private Set<Coordinate> frotier;
+    private Set<Coordinate> known;
+    private Coordinate[][] coordinates = new Coordinate[4][4];
+
+    class Coordinate {
+        final int x;
+        final int y;
+        final Coordinate left;
+        final Coordinate right;
+        final Coordinate up;
+        final Coordinate down;
+        List<Coordinate> neighbors;
+
+
+        public Coordinate(int x, int y){
+            this.x=x;
+            this.y=y;
+            neighbors=new ArrayList<>();
+            if (x>1){
+                left=coordinates[x-2][y-1];
+                neighbors.add(left);
+            }else {
+                left=null;
+            }
+
+            if (x<4){
+                right=coordinates[x][y-1];
+                neighbors.add(right);
+            }else {
+                right=null;
+            }
+
+            if (y<4){
+                up=coordinates[x-1][y];
+                neighbors.add(up);
+            }else {
+                up=null;
+            }
+
+            if (y>1){
+                down=coordinates[x-1][y-2];
+                neighbors.add(down);
+            }else {
+                down=null;
+            }
+
+        }
+
+        public Coordinate getLeft() {
+            return left;
+        }
+
+        public Coordinate getRight() {
+            return right;
+        }
+
+        public Coordinate getUp() {
+            return up;
+        }
+
+        public Coordinate getDown() {
+            return down;
+        }
+    }
     
     /**
      * Creates a new instance of your solver agent.
@@ -18,7 +89,15 @@ public class MyAgent implements Agent
      */
     public MyAgent(World world)
     {
-        w = world;   
+        w = world;
+        //initialize
+        for (int i=0; i<4; i++){
+            for (int j=0; j<4; j++)
+            coordinates[i][j]=new Coordinate(i+1, j+1);
+        }
+        known.add(coordinates[0][0]);
+        frotier.add(coordinates[0][1]);
+        frotier.add(coordinates[1][0]);
     }
    
             
@@ -114,7 +193,28 @@ public class MyAgent implements Agent
     {
       return (int)(Math.random() * 4);
     }
-    
-    
+
+    /**
+     * find way to a frontier root
+     */
+    private void moveTo(Coordinate destination){
+        //find path
+        Coordinate current = coordinates[w.getPlayerX()-1][w.getPlayerY()-1];
+        Stack<Coordinate> path = new Stack<>();
+        path.push(destination);
+        Coordinate next=null;
+        for (int i=0; i<destination.neighbors.size(); i++){
+            next=destination.neighbors.get(i);
+            if (w.isVisited(next.x, next.y)){
+                break;
+            }
+        }
+        while (!current.equals(next)){
+
+        }
+    }
+
 }
+
+
 
